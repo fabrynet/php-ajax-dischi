230f8c4209c7de9efba464c878ac9d9e7b8f8609
Fabrizio <fabrynet.bianco@gmail.com>
2020-09-07 18:30:28 +0200

    Sistemazione condizione noFilters con count($_GET)

--- server.php
+++ server.php
@@ -4,25 +4,18 @@ header('content-type:application/json');
 
 include 'database.php';
 
-$data = [];
-$noFilters = (is_null($_GET['author']) || $_GET['author'] === '')
-              && (is_null($_GET['genre']) || $_GET['genre'] === ''); //is_null mi restituisce vero solo se la variabile $_GET è null, evito il pericolo che mi restituisca vero se $_GET = false
-$authorFilter = !is_null($_GET['author']) && $_GET['author'] !== '';
+$hasGet = count($_GET) > 0; //controllo che abbia almeno un GET, GET è sempre un array
+$noFilters = !$hasGet || ($_GET['author'] === '' && $_GET['genre'] === '');
+// $noFilters = (is_null($_GET['author']) || $_GET['author'] === '')
+              // && (is_null($_GET['genre']) || $_GET['genre'] === '');
+$authorFilter = !is_null($_GET['author']) && $_GET['author'] !== ''; //is_null mi restituisce vero solo se la variabile $_GET è null, evito il pericolo che mi restituisca vero se $_GET = false
 $genreFilter = !is_null($_GET['genre']) && $_GET['genre'] !== '';
 
 $author = $_GET['author'];
 $genre = $_GET['genre'];
 
-// if (!$_GET['author'] || $_GET['author'] === '') {
-//   $data = $db;
-// } else {
-//   $author = $_GET['author'];
-//   foreach ($db['response'] as $cd) {
-//     if ($cd['author'] === $author) {
-//         $data['response'][] = $cd;
-//     }
-//   }
-// }
+$data = [];
+
 if ($noFilters) {
   $data = $db;
 } else if ($authorFilter && !$genreFilter) {
@@ -44,6 +37,17 @@ if ($noFilters) {
     }
   }
 }
+
+// if (!$_GET['author'] || $_GET['author'] === '') {
+//   $data = $db;
+// } else {
+//   $author = $_GET['author'];
+//   foreach ($db['response'] as $cd) {
+//     if ($cd['author'] === $author) {
+//         $data['response'][] = $cd;
+//     }
+//   }
+// }
 
 echo json_encode($data);
 
